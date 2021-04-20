c782c8a6fbf08937f560a94758a7d1c8ba24c17e
Jaime García <jgm00115@outlook.es>
2021-04-20 17:37:07 +0200

    correccion de documentacion

--- FCT.m
+++ FCT.m
@@ -1,7 +1,7 @@
 function C = FCT(x)
 % FCT obtiene la DCT de la señal de entrada a través de la FFT de una
 % secuencia del doble de muestras compuesta por la señal de entrada y 
-% su inversión temporal desplazada.
+% su inversión temporal desplazada, según el método de Makhoul.
 % INPUT:
 %     x:    señal de entrada
 % OUTPUT:
@@ -19,7 +19,9 @@ V = V(1:N);
 % Definimos los pesos correspondientes a cada coeficiente
 w = [sqrt(1/(4*N));sqrt(1/(2*N))*ones(N-1,1)];
 % Obtenemos la DCT multiplicando los coeficientes de la FFT anteriormente
-% seleccionados por los pesos (w) y aplicando un desplazamiento circular
+% seleccionados por los pesos (w) y aplicando un desplazamiento circular de
+% media muestra teniendo en cuenta que hemos analizado una secuencia de 2N
+% muestras
 k = (0:N-1)';
 C = w.*real(V.*exp((-j*pi*k)/(2*N)));
 end
